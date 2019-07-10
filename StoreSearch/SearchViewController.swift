934cf645eeda07d80e4c973d6a8428cdc4d4d19c
Joshua Cleetus <joshua.cleetus@gmail.com>
2019-07-10 18:39:07 -0400

    new project initial commit

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Macbook Air on 7/10/19.
+//  Copyright © 2019 Joshua Cleetus. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
